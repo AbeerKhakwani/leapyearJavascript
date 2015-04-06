89c2a92be7e54b380032cd9205ead73d8d41d8fb
Khakwani & de Vries <student@epicodus.com>
2015-04-06 14:34:36 -0700

    finish leap year

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var leapYear = function(year) {
+  if ((year % 4 === 0 ) && (year % 100 !== 0)){
+     return true;
+   }
+
+   else{
+
+   return false;
+   }
+};
+
+$(document).ready(function() {
+
+  $("form#leap-year").submit(function(event) {
+    //$(".not").remove();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+    if (!result) {
+    $(".not").text("not");
+    }
+    else{
+      $(".not").text("");
+
+    }
+
+    $("#result").show();
+
+    event.preventDefault();
+  });
+});
